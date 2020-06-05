772c30a04e88f7920e698f5e07ab1899c0c167dc
vyun <vyun@uwaterloo.ca>
2020-06-05 15:39:03 -0400

    dependencies with index.html

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  facebook_api_key: "576756513229634",
+  facebook_api_secret: "05c84b2b4ac466dec9724ce90665232d",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
